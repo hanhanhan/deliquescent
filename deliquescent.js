f2b35a6c7a0fef83c1e06a835dd467c4d773d78c
Han Lazarus <han.lazarus@gmail.com>
2016-12-16 13:08:08 -0800

    reorganize and rename 'frame' function math, parameterize line styling

--- deliquescent.js
+++ deliquescent.js
@@ -1,7 +1,8 @@
+//Deliquescent
+//the hanhanhan version
+//A fork of:
 //http://codepen.io/tmrDevelops/pen/OPZKNd
 //Tiffany Rayside
-//Deliquescent
-//modified by hanhanhan
 
 window.requestAnimFrame = (function(callback) {
   return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
@@ -17,22 +18,24 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
  //y height (canvas height)
-const w = 30; //grid sq width
-const h = 30; //grid sq height
+const w = 15; //grid sq width
+const h = 15; //grid sq height
 
 var rows = canvas.height/w; //number of rows
 var columns = canvas.width/w; //number of columns
 
-const P = 0.01; //Multiplier for offset between resting position and pulled point
-const n = 0.98; //n value for later
-const n_vel = 0.02; //velocity
-const C = 5; //Multiplier for line style as function of displacement
+const KX1 = 0.013; //X axus amplification - multiplier for difference between resting position and pulled point
+const KX2 = 0.025; //X axis decay
+const KY1 = 0.01; //Y axis amplification - multiplier for difference between resting position and pulled point
+const KY2 = 0.035; //decay
 
 var parts; //particles 
-var ŭ = 0; //color update
+var colorCycle = 0; //color offset which gets incremented with time
 var mouseX = 0; //mouse x
 var mouseY = 0; //mouse y
-var mouseDown = false; //mouse down flag
+var mouseDown = false; //mouse down flag\
+var displacementMax = 0;
+var colorScale = 1;
 
 var Part = function() {
   this.x = 0; //x pos
@@ -41,8 +44,9 @@ var Part = function() {
   this.vy = 0; //velocity y
   this.ind_x = 0; //index x
   this.ind_y = 0; //index y
-  this.displacement = 0;
-  this.off_dx = 0;
+  this.displacement = 0; //distance from resting position
+  this.off_dx = 0; //distance along x axis from resting position
+  this.off_dy = 0; //distance along y axis from resting position
 };
 
 Part.prototype.frame = function frame() {
@@ -59,6 +63,7 @@ Part.prototype.frame = function frame() {
   var off_dy = this.ind_y * h - this.y;
 
   this.off_dx = off_dx;
+  this.off_dy = off_dy;
   this.displacement = Math.sqrt(off_dx * off_dx + off_dy * off_dy);
 
   var ax = 0;
@@ -76,12 +81,12 @@ Part.prototype.frame = function frame() {
   ax -= this.x - parts[this.ind_x][this.ind_y + 1].x;
   ay -= this.y - parts[this.ind_x][this.ind_y + 1].y;
 
-  //decaying damping
-  this.vx += P * ax - this.vx * n_vel;
-  this.vy += P * ay - this.vy * n_vel;
+  //amplification * net pull - decaying damping
+  this.vx += KX1 * ax - KX2 * this.vx;
+  this.vy += KY1 * ay - KY2 * this.vy;
 
-  this.x += this.vx * n;
-  this.y += this.vy * n;
+  this.x += this.vx;
+  this.y += this.vy;
 
   if (mouseDown) {
     var dx = this.x - mouseX;
@@ -96,13 +101,30 @@ Part.prototype.frame = function frame() {
 };
 
 Part.prototype.displacementStyle = function displacementStyle(){
-  var hue = ŭ + C * this.displacement;
+  //note: displacement is always positive,
+  //off_dx and off_dy are positive and negative
+
+  //hue is offset by a cycling color, in a 120 deg window
+  var hue = colorCycle + this.displacement * 120 / colorScale;
+
+  var saturation_offset = 40;
+  var saturation = saturation_offset + this.displacement / colorScale;
+  saturation = saturation > 90 ? 90 : saturation;
+  saturation = saturation < 40 ? 40 : saturation; 
+  saturation = saturation + "%"; 
+
+  var lightness_offset = 60;
+  var lightness = lightness_offset + this.off_dy / colorScale;
+  lightness = lightness > 80 ? 80 : lightness;
+  lightness = lightness < 40 ? 40 : lightness; 
+  lightness = lightness + "%"; 
+
   var alpha_offset = 0.6;
   var alpha = alpha_offset + this.off_dx;
   alpha = alpha > 1 ? 1 : alpha;
   alpha = alpha < 0.2 ? 0.2 : alpha;
 
-  context.strokeStyle = 'hsla(' + hue + ', 100%, 80%, ' + alpha +')';
+  context.strokeStyle = 'hsla(' + hue + ',' + saturation + ', ' + lightness + ', ' + alpha +')';
   context.lineWidth = 1 + this.displacement * 0.03;
   context.beginPath();
 }
@@ -125,13 +147,24 @@ function initializeArray() {
 //draw grid 
 function draw(i,j) {
   var p = parts[i][j];
-  var pAcross = parts[i][j + 1]; //across row
-  var pDown = parts[i + 1][j]; //down column
+  // these commented lines are fun to play with -- change drawing patterns
+  // var pAcross = parts[i][j + 1]; //across row
+  // var pDown = parts[i + 1][j]; //down column
+
+  // context.moveTo(p.x, p.y);
+  // context.lineTo(pAcross.x, pAcross.y); 
+  // context.moveTo(p.x, p.y);
+  // context.lineTo(pDown.x, pDown.y); 
+  var pUp = parts[i][j - 1];
+  var pDownRight = parts[i + 1][j + 1];
+  var pDownLeft = parts[i - 1][j + 1];
 
   context.moveTo(p.x, p.y);
-  context.lineTo(pAcross.x, pAcross.y); 
+  //context.lineTo(pUp.x, pUp.y); 
   context.moveTo(p.x, p.y);
-  context.lineTo(pDown.x, pDown.y); 
+  context.lineTo(pDownRight.x, pDownRight.y); 
+  context.moveTo(p.x, p.y);
+  context.lineTo(pDownLeft.x, pDownLeft.y); 
 }
 
 function resize() {
@@ -160,18 +193,21 @@ window.onload = function() {
     //wipe canvas
     context.fillStyle = "hsla(0, 5%, 5%, .1)";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    ŭ -= 0.5;
-
+    colorCycle -= 1;
+    displacementMax = 0;
     //looping through array of points
-    for (var i = 0; i < columns - 1; i++) {
-      for (var j = 0; j < rows - 1; j++) {
+    for (var i = 1; i < columns - 1; i++) {
+      for (var j = 1; j < rows - 1; j++) {
         var p = parts[i][j];
         context.beginPath();
         p.frame();
         p.displacementStyle();
         draw(i,j);
         context.stroke();
+        //displacementMax < p.displacement && p.displacement = displacementMax;
+        displacementMax = displacementMax > p.displacement ? displacementMax : p.displacement 
       }
+      colorScale = displacementMax;
     }
     window.requestAnimFrame(run);
   }
